c2554116b683645414e4b8cf91861c1ad8470b1e
Lech Kulina <kulinalech@gmail.com>
2016-10-12 20:39:57 +0200

    Improve LogsConsoleSink initialization and set cerr as a default stream

--- engine/source/Application/LogsConsoleSink.cpp
+++ engine/source/Application/LogsConsoleSink.cpp
@@ -5,35 +5,64 @@
  * For conditions of distribution and use, see copyright details in the LICENSE file.
  */
 #include <iostream>
+#include <algorithm>
+#include <boost/bind.hpp>
+#include <boost/range.hpp>
 #include "LogsConsoleSink.h"
 
+namespace ros {
+
+    static const struct StreamMapping {
+       const char* str;
+       std::ostream* stream;
+    } streamMappings[] = {
+        {"Output", &std::cout},
+        {"Error", &std::cerr},
+        {"Log", &std::clog}
+    };
+
+    static std::ostream* Stream_FromString(const char* str) {
+        const StreamMapping* iter = std::find_if(boost::begin(streamMappings), boost::end(streamMappings),
+                                                   boost::bind(strcmp, boost::bind(&StreamMapping::str, _1), str) == 0);
+        if (iter != boost::end(streamMappings)) {
+            return iter->stream;
+        }
+        return ROS_NULL;
+    }
+
+}
+
 ros::LogsConsoleSink::LogsConsoleSink()
-    : stream(&std::cerr) {
+    : stream(ROS_NULL) {
 }
 
 bool ros::LogsConsoleSink::Init(const PropertyTree& config) {
     if (!LogsSink::Init(config)) {
         return false;
     }
-    StringOpt streamConfig = config.get_optional<std::string>("Stream");
-    if (streamConfig) {
-        if (*streamConfig == "Output") {
-            stream = &std::cout;
-        } else if (*streamConfig == "Error") {
-            stream = &std::cerr;
-        } else {
-            std::cerr << "Unknown stream " << *streamConfig << " found in standard output sink config" << std::endl;
-        }
+
+    String streamConfig = config.get<String>("Stream", "Error");
+    std::ostream* streamMapped = Stream_FromString(streamConfig.c_str());
+    if (!streamMapped) {
+        std::cerr << "Failed to initialize console sink: Unknown stream " << streamConfig << std::endl;
+        return false;
     }
+    stream = streamMapped;
+
     return true;
 }
 
 bool ros::LogsConsoleSink::SendMessage(const LogMessage& message) {
+    if (!stream) {
+        return false;
+    }
     (*stream) << message;
     return stream->good();
 }
 
 void ros::LogsConsoleSink::FlushMessages() {
-    stream->flush();
+    if (stream) {
+        stream->flush();
+    }
 }
 
