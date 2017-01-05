c59ec895e1bc653e65befcd33ce53b70424b599c
Lech Kulina <kulinalech@gmail.com>
2017-01-05 08:31:01 +0100

    Implement factory for ArchiveFiles

--- engine/include/resources/ArchiveFile.h
+++ engine/include/resources/ArchiveFile.h
@@ -9,21 +9,28 @@
 
 #include <core/Common.h>
 #include <core/Environment.h>
+#include <core/Factory.h>
 #include <core/File.h>
 #include <resources/ArchiveEntry.h>
 
 namespace ros {
     class ArchiveFile;
     typedef boost::shared_ptr<ArchiveFile> ArchiveFilePtr;
+    typedef Factory<ArchiveFile> ArchiveFileFactory;
 
     class ROS_API ArchiveFile: public File {
         public:
+            static ArchiveFilePtr create(const char* path);
+
             virtual ~ArchiveFile() {}
 
             virtual bool open(const char* path) =0;
             virtual void close() =0;
 
             virtual const ArchiveEntryMap& getEntries() const =0;
+
+        private:
+            static ArchiveFileFactory factory;
     };
 }
 
